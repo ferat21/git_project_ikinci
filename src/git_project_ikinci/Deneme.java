22c8d60ae144231c43e2a6cf01cecd87c5c159fc
firat ulas <fulas@gmail.com>
2020-11-04 16:58:14 +0100

    ikinci satir ekledim

--- src/git_project_ikinci/Deneme.java
+++ src/git_project_ikinci/Deneme.java
@@ -3,8 +3,9 @@ package git_project_ikinci;
 public class Deneme {
 
 	public static void main(String[] args) {
-		System.out.println("ikinci git projem");
-
+		System.out.println("ikinci git projem 1");
+		System.out.println("ikinci satir kodum");
+		
 	}
 
 }
